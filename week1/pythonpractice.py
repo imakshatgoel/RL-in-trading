90d5ae3bffc0401f14a3a634a34b97af07f1df2c
jain2308 <mayankjain9013@gmail.com>
2025-12-09 16:59:03 +0530

    Add files via upload

---
+++ week1/pythonpractice.py
@@ -0,0 +1,12 @@
+#PROBLEM 1
+# take a numerical array as input and output sum of its elememts
+#PROBLEM 2
+# take a numerical array as input and output maximum number
+# PROBLEM 3
+# take a sentence as input and output the number of vowels in it
+# PROBLEM 4
+# take a person's name and occupation as input and output "Hi {name}, you are a/an {occupation}"
+# PROBLEM 5
+# take an integer > 1 as input and output whether it is prime or nor
+# PROBLEM 6
+# take a string and print yes if it is a palindrome(GOOGLE THIS) or no otherwise
